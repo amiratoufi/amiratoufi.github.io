7c0d081f6ae5b2f76a4d7c92149f8add5c236d65
amiratoufi <amir.atoufi@gmail.com>
2024-10-30 00:21:28 +0000

    fix: cv

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -9,7 +9,18 @@ const HomePage = () => {
   return (
     <Container className="flex flex-col">
       <div className="flex flex-col mt-4 md:flex-row bg-white rounded-md">
-        <div className="md:w-2/6">
+        {/* For small screens - Centered Image */}
+        <div className="block md:hidden mx-auto">
+          <Image
+            className=" mx-auto"
+            width={150} // Smaller size for mobile
+            src={portfolio}
+            alt="Portfolio"
+          />
+        </div>
+
+        {/* For larger screens - Original Image Layout */}
+        <div className="hidden md:block md:w-2/6">
           <Image
             className="rounded-tl-md rounded-bl-md"
             width={300}
@@ -17,44 +28,57 @@ const HomePage = () => {
             alt="Portfolio"
           />
         </div>
+
         <div className="md:w-4/6 flex flex-col justify-center mt-4 md:mt-0">
           <h1 className="text-4xl italic mt-4 text-center md:text-left">
             Amir Atoufi
           </h1>
           <p className="mt-4 text-center md:text-left p-4">
-          I am a Research Associate in the Department of Earth Sciences at the University of Cambridge, specializing in geophysical fluid dynamics with a focus on boundary layer processes. My research combines theory, numerical simulations, laboratory experiments, and mathematical analysis to better understand complex geophysical flows. Before joining Earth Sciences, I worked as a postdoctoral research associate in the Department of Applied Mathematics and Theoretical Physics (DAMTP) as part of the Atmosphere-Ocean Dynamics Group and the G.K. Batchelor Laboratory.
-
-I completed my Ph.D. at the University of Waterloo, where I focused on turbulence in stably stratified boundary layers, particularly in contexts similar to nocturnal atmospheric boundary layers. During my postdoctoral studies at DAMTP, I contributed to the ERC-funded STAMP (Stratified Turbulence and Mixing Processes) project, investigating the relationship between internal hydraulics and mixing in stratified exchange flows, such as those found in estuaries and straits.
-
-My current research centers on abyssal ocean turbulence, specifically exploring interactions between tides and bottom boundary layer processes, including upwelling and downwelling. I work closely with DAMTP colleagues to deepen our understanding of these crucial oceanic processes.
+            I am a Research Associate in the Department of Earth Sciences at the
+            University of Cambridge, specializing in geophysical fluid dynamics
+            with a focus on boundary layer processes. My research combines
+            theory, numerical simulations, laboratory experiments, and
+            mathematical analysis to better understand complex geophysical
+            flows. Before joining Earth Sciences, I worked as a postdoctoral
+            research associate in the Department of Applied Mathematics and
+            Theoretical Physics (DAMTP) as part of the Atmosphere-Ocean Dynamics
+            Group and the G.K. Batchelor Laboratory.
+            <br />
+            <br />
+            I completed my Ph.D. at the University of Waterloo, where I focused
+            on turbulence in stably stratified boundary layers, particularly in
+            contexts similar to nocturnal atmospheric boundary layers. During my
+            postdoctoral studies at DAMTP, I contributed to the ERC-funded STAMP
+            (Stratified Turbulence and Mixing Processes) project, investigating
+            the relationship between internal hydraulics and mixing in
+            stratified exchange flows, such as those found in estuaries and
+            straits.
+            <br />
+            <br />
+            My current research centers on abyssal ocean turbulence,
+            specifically exploring interactions between tides and bottom
+            boundary layer processes, including upwelling and downwelling.
           </p>
           <p className="text-center md:text-left p-4">
-          I hold an MSc in Aerodynamics from K. N. Toosi University of Technology, where my thesis focused on compressible turbulent mixing using large eddy simulations (LES). I also have a BSc in Mechanical Engineering. After completing my Master’s, I worked in the offshore oil and gas industry, gaining broad experience in roles such as mechanical engineer, projects coordinator, hydrodynamic analyst, computational fluid dynamics (CFD) engineer, engineering coordinator, and project manager.
+            I hold an MSc in Aerodynamics from K. N. Toosi University of
+            Technology, where my thesis focused on compressible turbulent mixing
+            using large eddy simulations (LES). I also have a BSc in Mechanical
+            Engineering. After completing my Master’s, I worked in the offshore
+            oil and gas industry, gaining broad experience in roles such as
+            mechanical engineer, projects coordinator, hydrodynamic analyst,
+            computational fluid dynamics (CFD) engineer, engineering
+            coordinator, and project manager.
           </p>
           <div className="text-center md:text-left font-semibold p-4">
-            <button
-              type="button"
-              className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-              onClick={() => {
-                window.location.href =
-                  "https://www.dropbox.com/scl/fi/d7t68kjr04qn0umrair9p/Amir-Atoufi-CV.pdf?rlkey=509tsor3977auetr84z6el2t8&dl=0";
-              }}
-            >
-              CV
-            </button>
-            {/* <DropdownCV /> */}
+            <a href="/cv/amir_atoufi-cv.pdf" download>
+              View or Download CV
+            </a>
           </div>
-          {/* <Link
-            className="text-center md:text-left font-semibold p-4 hover:text-blue-700"
-            href="https://www.dropbox.com/scl/fi/d7t68kjr04qn0umrair9p/Amir-Atoufi-CV.pdf?rlkey=509tsor3977auetr84z6el2t8&dl=0"
-          >
-            CV
-          </Link> */}
         </div>
       </div>
-      <hr className="my-12 w-5/6 mx-auto  h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
+      <hr className="my-12 w-5/6 mx-auto h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
       <Education />
-      <hr className="my-12 w-5/6 mx-auto  h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
+      <hr className="my-12 w-5/6 mx-auto h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
       <Experience />
     </Container>
   );
--- src/pages/research/index.tsx
+++ src/pages/research/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
@@ -8,30 +7,20 @@ import rehypeKatex from 'rehype-katex';
 import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
-import {unified} from 'unified';
+import { unified } from 'unified';
 import Hero from "@/components/Home/Hero";
 import heroImage from "../../images/gibraltar_amo_2017067_lrg.jpg";
 import Link from "next/link";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Layout from '@/components/Layout';
 import Container from '@/components/Layout/Container';
 import Head from 'next/head';
+import { Spinner } from '@cloudscape-design/components';
 
+const Research = ({ researchData }:any) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedImages, setLoadedImages] = useState(0);
 
-interface ResearchDataProps {
-  title: string,
-  hero: string,
-  images?: string[],
-  link: string,
-  content?: string,
-  date?:Date 
-}
-
-interface Props {
-  researchData: ResearchDataProps[]
-}
-
-const Research = ({ researchData }: Props) => {
   useEffect(() => {
     const init = async () => {
       const { Ripple, initTE } = await import("tw-elements");
@@ -40,62 +29,63 @@ const Research = ({ researchData }: Props) => {
     init();
   }, []);
 
-  const toKebabCase = (str:string) => {
-    return str.toLowerCase().replace(/\s+/g, '-');
+  const handleImageLoad = () => {
+    setLoadedImages((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    if (loadedImages === researchData.length) {
+      setIsLoading(false);
+    }
+  }, [loadedImages, researchData.length]);
+
+  const toKebabCase = (str:string) => str.toLowerCase().replace(/\s+/g, '-');
+
   return (
     <>
-       <Head> <title>Research</title></Head>
-  
-    <Layout className="pt-4">
-      <Hero heroUrl={heroImage.src} heroText="Research" />
-      <Container>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mt-4">
-          {researchData.map((item: ResearchDataProps) => (
-            <Link
-              key={item.title}
-              href={`/research/${encodeURIComponent(toKebabCase(item.title))}`}
-              passHref
-            >
-              <div className="block mb-4 rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                <div
-                  className="relative overflow-hidden bg-cover bg-no-repeat"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  <img
-                    className="rounded-t-lg w-full h-[200px]"
-                    src={`/images/${item.hero}`}
-                    alt=""
-                  />
-                  <div>
-                    <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+      <Head>
+        <title>Research</title>
+      </Head>
+      {isLoading && <Spinner />}
+      <Layout className={`${isLoading ? 'hidden' : 'pt-4'}`}>
+        <Hero heroUrl={heroImage.src} heroText="Research" />
+        <Container>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mt-4">
+            {researchData.map((item:any) => (
+              <Link
+                key={item.title}
+                href={`/research/${encodeURIComponent(toKebabCase(item.title))}`}
+                passHref
+              >
+                <div className="block mb-4 rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+                  <div className="relative overflow-hidden bg-cover bg-no-repeat">
+                    <img
+                      className="rounded-t-lg w-full h-[200px]"
+                      src={`/images/${item.hero}`}
+                      alt=""
+                      onLoad={handleImageLoad}
+                    />
+                    <div className="absolute inset-0 h-full w-full bg-gray-900 bg-opacity-25 opacity-0 hover:opacity-100 transition duration-300 ease-in-out"></div>
+                  </div>
+                  <div className="p-6">
+                    <div className="h-20">
+                      <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 overflow-hidden overflow-ellipsis">
+                        {item.title}
+                      </h5>
+                    </div>
+                    <div className="mb-4 line-clamp-4 text-base text-neutral-600 dark:text-neutral-200">
+                      <div dangerouslySetInnerHTML={{ __html: item.content || '' }} />
+                    </div>
+                    <div className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow transition duration-150 ease-in-out hover:bg-primary-600 focus:bg-primary-600 focus:outline-none dark:shadow dark:hover:bg-primary-700">
+                      Read more
+                    </div>
                   </div>
                 </div>
-                <div className="p-6">
-                  <div className="h-20">
-                    <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 overflow-hidden overflow-ellipsis">
-                      {item.title}
-                    </h5>
-                  </div>
-                  <div className="mb-4 line-clamp-4 text-base text-balance text-neutral-600 dark:text-neutral-200">
-                    {/* Render HTML directly */}
-                    <div dangerouslySetInnerHTML={{ __html: item.content || '' }} />
-                  </div>
-                  <div
-                    className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                  >
-                    Read more
-                  </div>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </Container>
-    </Layout>
+              </Link>
+            ))}
+          </div>
+        </Container>
+      </Layout>
     </>
   );
 };
@@ -119,22 +109,19 @@ export async function getStaticProps() {
         .process(content);
 
       const hero = data.hero || '';
-
-      // Parse month and year from metadata and format it as a serializable string
       const [month, year] = (data.date || '').split(' ');
-      const date = new Date(`${year}-${month}-01`).toISOString(); 
+      const date = new Date(`${year}-${month}-01`).toISOString();
 
       return {
         title: data.title,
         hero: hero,
         content: String(processedContent),
         link: data.link,
-        date: date, 
+        date: date,
       };
     })
   );
 
-  // Sort researchData by date in descending order (newest first)
   researchData.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
   return {
